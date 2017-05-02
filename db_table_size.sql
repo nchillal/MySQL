b75ab2eab65077da9ef26a776273f47c669a94e3
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-05-01 19:59:01 -0700

    Changes

--- db_table_size.sql
+++ db_table_size.sql
@@ -1,5 +1,5 @@
 SELECT    table_name AS "Table",
-          ROUND(((data_length + index_length) / 1024 / 1024), 2) AS "Size (MB)"
+          ROUND(((data_length + index_length)/1024/1024), 2) AS "Size (MB)"
 FROM      information_schema.TABLES
 WHERE     table_schema = "dbname"
 ORDER BY  (data_length + index_length) DESC;
